e875c0817e22c335a40051392170ebdf73648168
Leidy Melissa Trejos Pamplona <leidy.meli14@gmail.com>
2023-11-20 16:38:26 -0500

    modify the project structure and add a database context

---
+++ API_AppParkingSoft/DAL/AuditBase.cs
@@ -0,0 +1,9 @@
+﻿namespace API_AppParkingSoft.DAL.Entities
+{
+    public class AuditBase
+    {
+        [Key]
+        [Required]
+        public virtual Guid Id { get; set; }
+    }
+}
---
+++ API_AppParkingSoft/DAL/DataBaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API_AppParkingSoft.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
