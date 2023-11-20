49b312c8fe5a12e830650b2d61f64f93ca20a3c8
FERNEY ORLANDO LOPEZ COPETE <ferneylopez188523@correo.itm.edu.co>
2023-11-20 17:10:25 -0500

    Connection Strings in .json

---
+++ API_AppParkingSoft/DAL/Entities/AuditBase.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_AppParkingSoft.DAL.Entities
+{
+    public class AuditBase
+    {
+        [Key] 
+        [Required] 
+
+        public virtual Guid Id { get; set; }
+
+        public virtual DateTime? CreatedDate { get; set; } 
+
+        public virtual DateTime? ModifiedDate { get; set; }
+    }
+}
