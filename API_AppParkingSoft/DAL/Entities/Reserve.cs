d0c67223f1c6ef135170ca64dbee4e5174b84a91
FERNEY ORLANDO LOPEZ COPETE <ferneylopez188523@correo.itm.edu.co>
2023-11-21 19:19:54 -0500

    update entities.

--- API_AppParkingSoft/DAL/Entities/Reserve.cs
+++ API_AppParkingSoft/DAL/Entities/Reserve.cs
@@ -9,7 +9,13 @@ namespace API_AppParkingSoft.DAL.Entities
 
         public DateTime ? FinalDate { get; set; }
 
+        public DateTime StartHour { get; set; }
+
+        public DateTime? FinalHour { get; set; }
+
         public double TotalCost { get; set; }
+
+        public bool stateReserve { get; set; }
 
         //Vehicle type
         [Display(Name = "Vehicle")]
