2fdf6839cadd1dc2fc20667a023dd85adb804389
Leidy Melissa Trejos Pamplona <leidy.meli14@gmail.com>
2023-11-22 11:25:16 -0500

    Client Interface, service and controller

---
+++ API_AppParkingSoft/Controllers/ClientsController.cs
@@ -0,0 +1,100 @@
+﻿using API_AppParkingSoft.DAL.Entities;
+using API_AppParkingSoft.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_AppParkingSoft.Controllers
+{
+    public class ClientsController : Controller
+    {
+        private readonly IClientService _clientService;
+        public ClientsController(IClientService clientService)
+        {
+            _clientService = clientService;
+        }
+
+
+        [HttpGet, ActionName("Get")]
+        [Route("Get")]
+        public async Task<ActionResult<IEnumerable<Client>>> GetClientsAsync()
+        {
+            var clients = await _clientService.GetClientsAsync();
+
+            if (clients == null || !clients.Any()) return NotFound();
+
+            return Ok(clients);
+        }
+
+        [HttpPost, ActionName("Create")]
+        [Route("Create")]
+        public async Task<ActionResult<Client>> CreateClientAsync(Client client)
+        {
+            try
+            {
+                var createdClient = await _clientService.CreateClientAsync(client);
+
+                if (createdClient == null) return NotFound();
+
+                return Ok(createdClient);
+            }
+            catch (Exception ex)
+            {
+
+                if (ex.Message.Contains("duplicated"))
+                {
+                    return Conflict(String.Format("El país {0} ya existe.", client.Id));
+
+                }
+
+                return Conflict(ex.Message);
+            }
+        }
+
+        [HttpGet, ActionName("Get")]
+        [Route("GetById/{id}")]
+        public async Task<ActionResult<IEnumerable<Client>>> GetClientsByIdAsync(Guid id)
+        {
+            if (id == null) return BadRequest("Id es requerido!");
+
+            var country = await _clientService.GetClientsByIdAsync(id);
+
+            if (country == null) return NotFound();
+
+            return Ok(country);
+        }
+
+        [HttpPut, ActionName("Edit")]
+        [Route("Edit")]
+        public async Task<ActionResult<Client>> EditClientAsync(Client client)
+        {
+            try
+            {
+                var editedClient = await _clientService.EditClientAsync(client);
+
+                return Ok(editedClient);
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("duplicated"))
+                {
+                    return Conflict(String.Format("El cliente {0} ya existe.", client.Id));
+
+                }
+
+                return Conflict(ex.Message);
+            }
+        }
+
+        [HttpDelete, ActionName("Delete")]
+        [Route("Delete")]
+        public async Task<ActionResult<Client>> DeleteClientAsync(Guid id)
+        {
+            if (id == null) return BadRequest("Id es requerido");
+
+            var deleteCountry = await _clientService.DeleteClientAsync(id);
+
+            if (deleteCountry == null) return NotFound("Cliente no encontrado");
+
+            return Ok(deleteCountry);
+        }
+    }
+}
---
+++ API_AppParkingSoft/Domain/Interfaces/IClientService.cs
@@ -0,0 +1,13 @@
+﻿using API_AppParkingSoft.DAL.Entities;
+
+namespace API_AppParkingSoft.Domain.Interfaces
+{
+    public interface IClientService
+    {
+        Task<IEnumerable<Client>> GetClientsAsync();
+        Task<Client> CreateClientAsync(Client client);
+        Task<Client> GetClientsByIdAsync(Guid id);
+        Task<Client> EditClientAsync(Client client);
+        Task<Client> DeleteClientAsync(Guid id);
+    }
+}
