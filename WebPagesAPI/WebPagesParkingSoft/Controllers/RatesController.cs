d841f41374ce06e3111a6e30e66d4b62068ed2ba
Leidy Melissa Trejos Pamplona <leidy.meli14@gmail.com>
2023-11-24 12:03:40 -0500

    Init web pages

---
+++ WebPagesAPI/WebPagesParkingSoft/Controllers/RatesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
+using System.Net.Http;
+using Newtonsoft.Json;
+using API_AppParkingSoft.DAL.Entities;
+
+namespace WebPagesParkingSoft.Controllers
+{
+    public class RatesController : Controller
+    {
+        private readonly IHttpClientFactory _httpClient;
+        public RatesController(IHttpClientFactory httpClient) {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var url = "https://localhost:7211/api/Rate/GetRate";
+            var json = await _httpClient.CreateClient().GetStringAsync(url);
+            List<Rate> rates = JsonConvert.DeserializeObject<List<Rate>>(json);
+
+            return View(json);
+        }
+    }
+}
